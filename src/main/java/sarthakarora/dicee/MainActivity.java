a9a15eff23499c04218f0384ac971777e38697ca
sarora96 <sa3522@columbia.edu>
2017-07-13 18:17:12 -0400

    Finished onClick method.

--- src/main/java/sarthakarora/dicee/MainActivity.java
+++ src/main/java/sarthakarora/dicee/MainActivity.java
@@ -1,9 +1,15 @@
 package sarthakarora.dicee;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +20,24 @@ public class MainActivity extends AppCompatActivity {
 
         Button roll = (Button) findViewById(R.id.rollButton);
 
+        final int [] diceArray = {R.drawable.dice1, R.drawable.dice2,
+                R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
+
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("Dicee", "Ze Button has been pressed!");
+
+                Random random = new Random();
+
+                int num = random.nextInt(6);
+                leftDice.setImageResource(diceArray[num]);
+                num = random.nextInt(6);
+                rightDice.setImageResource(diceArray[num]);
+                Log.d("Dicee", "The random number is " + num);
 
             }
         });
