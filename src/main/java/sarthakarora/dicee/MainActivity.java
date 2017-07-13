d6a5e691e978c28d4bf537576e296916d7a9a241
sarora96 <sa3522@columbia.edu>
2017-07-13 18:21:50 -0400

    Finished onClick method.

--- src/main/java/sarthakarora/dicee/MainActivity.java
+++ src/main/java/sarthakarora/dicee/MainActivity.java
@@ -30,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Random random = new Random();
+
+                // This is a simple comment!
+                
                 int num = random.nextInt(6);
                 leftDice.setImageResource(diceArray[num]);
                 num = random.nextInt(6);
